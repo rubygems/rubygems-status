71eb23e5d5df822665e6ff0ee6381d57081c1f25
Evan Phoenix <evan@phx.io>
2012-03-05 11:44:53 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubygemsStatus::Application.config.secret_token = '61c221dc9b7be04472f29049ba2f0219aeb73aa286b08630d7115811d40bcc62faca8b90f1611f25c83f37de601feb8da7ca31021bc3419febae7e5e2bfa541e'
