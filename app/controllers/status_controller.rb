dec19385ebb9094aa0387233a25fce7568b028b1
Evan Phoenix <evan@fallingsnow.net>
2012-03-05 18:30:25 -0800

    Handle unknown and partial statuses

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,14 +1,15 @@
 class StatusController < ApplicationController
   def show
     @core = Ping.find_by_service("Core API")
+    @pings = Ping.all.sort_by { |x| x.service }
+
     if !@core
       @status = "unknown"
+    elsif @core.status == "down"
+      @status = "down"
     else
-      @status = @core.status
+      @status = @pings.any? { |x| x.down? } ? "partial" : "up"
     end
-
-    @status = "up"
-    @pings = Ping.all.sort_by { |x| x.service }
   end
 
   def system
--- app/models/ping.rb
+++ app/models/ping.rb
@@ -1,2 +1,5 @@
 class Ping < ActiveRecord::Base
+  def down?
+    status == "down"
+  end
 end
