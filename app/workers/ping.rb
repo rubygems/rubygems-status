64820b1c9379ecd1e85c2229a32c0bbdbf49f52b
Evan Phoenix <evan@phx.io>
2012-03-14 11:56:11 -0700

    Add partial status and load balancers

--- app/workers/ping.rb
+++ app/workers/ping.rb
@@ -11,13 +11,18 @@ class PingWorker
     $1.to_i != 5 if $1
   end
 
-  def update_status(flag, service)
+  def update_raw_status(status, service)
     x = Ping.find_by_service service
+
     x = Ping.new :service => service unless x
 
-    x.status = (flag ? "up" : "down")
+    x.status = status
     x.last_seen = Time.now
     x.save
+  end
+
+  def update_status(flag, service)
+    update_raw_status(flag ? "up" : "down", service)
   end
 
   def update
@@ -30,12 +35,21 @@ class PingWorker
     s3 = TCPSocket.new "rubygems.org", 80
     s3 << "HEAD /api/v1/downloads HTTP/1.0\r\nHost: rubygems.org\r\n\r\n"
 
+    s4 = TCPSocket.new "f1.rubygems.org", 80
+    s4 << "HEAD /latest_specs.4.8.gz HTTP/1.0\r\nHost: rubygems.org\r\n\r\n"
+
+    s5 = TCPSocket.new "f2.rubygems.org", 80
+    s5 << "HEAD /latest_specs.4.8.gz HTTP/1.0\r\nHost: rubygems.org\r\n\r\n"
+
     @core_api = false
     @app = false
     @v1_api = false
     @push_api = false
+    @lb = 0
+    @f1 = false
+    @f2 = false
 
-    sockets = [s, s2, s3]
+    sockets = [s, s2, s3, s4, s5]
 
     until sockets.empty?
       r, _, _ = IO.select(sockets, nil, nil, @timeout)
@@ -48,6 +62,12 @@ class PingWorker
         when s3
           @v1_api = check_socket(s3)
           @push_api = @v1_api
+        when s4
+          @f1 = check_socket(s4)
+          @lb += 1 if @f1
+        when s5
+          @f2 = check_socket(s5)
+          @lb += 1 if @f2
         end
       end
 
@@ -58,5 +78,16 @@ class PingWorker
     update_status @app, "Application"
     update_status @push_api, "Push API"
     update_status @v1_api, "V1 API"
+
+    status = case @lb
+             when 0
+               "down"
+             when 1
+               "partial"
+             else
+               "up"
+             end
+
+    update_raw_status status, "Load Balancers"
   end
 end
