da8858d9b1db8797e76d9880377187d73b025c2e
Erik Michaels-Ober <sferik@gmail.com>
2013-12-01 16:23:08 +0100

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20120306045347) do
   create_table "pings", :force => true do |t|
     t.string   "service"
     t.string   "status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.boolean  "critical"
     t.datetime "last_seen"
     t.text     "description"
